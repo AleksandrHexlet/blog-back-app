0e19f73a2e41bf49078e49d453a5368076e010df
Alexandr <AlexandrDHL@yandex.ru>
2025-11-05 21:49:14 +0300

    init work version

---
+++ src/main/java/ru.yandex.practicum/controller/HomeController.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.controller; // Класс находится в пакете с контроллерами
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // Указываем Spring, что этот компонент является контроллером
+public class HomeController {
+
+    @GetMapping("/home") // Принимаем GET-запрос по адресу /home
+    @ResponseBody        // Указываем, что возвращаемое значение является ответом
+    public String homePage() {
+        return "<h1>Hello, world form HomeController!</h1>"; // Ответ
+    }
+    @GetMapping("/")
+    @ResponseBody
+    public String rootPage() {
+        return "Welcome to root";
+    }
+
+
+}
