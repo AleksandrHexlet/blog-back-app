a200cda4dc5c7faf5b03f882b3e096f575cbc465
Alexandr <AlexandrDHL@yandex.ru>
2025-11-06 22:37:27 +0300

    feat: Add Comment model class with Lombok annotations

---
+++ src/main/java/ru.yandex.practicum/model/Comment.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    private Long id;
+    private String text;
+    private Long postId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
